8efd0cbf24a2f1ac2f0847c77481f4ede44663d1
lugard1 <agulugard@gmail.com>
2022-10-18 17:54:13 -0700

    updates on index.html style.css index.js files

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-let hambuga = document.querySelector('.hambuger');
-let mobile = document.querySelector('.mobile-menu');
-let close = document.querySelector('.close');
-let body = document.querySelector('body');
-let live = document.querySelector('.list-item');
-let live2 = document.querySelector('.list-item2');
-let live3 = document.querySelector('.list-item3');
+const hambuga = document.querySelector('.hambuger');
+const mobile = document.querySelector('.mobile-menu');
+const close = document.querySelector('.close');
+const body = document.querySelector('body');
+const live = document.querySelector('.list-item');
+const live2 = document.querySelector('.list-item2');
+const live3 = document.querySelector('.list-item3');
 
-hambuga.addEventListener('click', ()=>{
+hambuga.addEventListener('click', () => {
   mobile.classList.add('show');
   body.classList.add('hide');
 });
-close.addEventListener('click', ()=>{
-mobile.classList.remove('show');
-body.classList.remove('hide');
-});
-/*live.forEach(('item')=> {*/
-
-  live.addEventListener('click', () =>{
+close.addEventListener('click', () => {
   mobile.classList.remove('show');
   body.classList.remove('hide');
-  });
+});
 
-  live2.addEventListener('click', () =>{
-    mobile.classList.remove('show');
-    body.classList.remove('hide');
-  });
+live.addEventListener('click', () => {
+  mobile.classList.remove('show');
+  body.classList.remove('hide');
+});
 
-  live3.addEventListener('click', () =>{
-    mobile.classList.remove('show');
-    body.classList.remove('hide');
-    });
+live2.addEventListener('click', () => {
+  mobile.classList.remove('show');
+  body.classList.remove('hide');
+});
+
+live3.addEventListener('click', () => {
+  mobile.classList.remove('show');
+  body.classList.remove('hide');
+});
