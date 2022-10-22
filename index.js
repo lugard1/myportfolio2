496f6861cad1971ee1f02bb2566373d3a2ebbe1f
lugard1 <agulugard@gmail.com>
2022-10-21 19:56:35 -0700

    made changes to dynamically add the popup modal

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 const hamburger = document.querySelector('#hamburger');
 const navBar = document.querySelector('#mobile-navbar');
 const headline = document.querySelector('#headline');
-
 const body = document.querySelector('body');
-
 const projectsDiv = document.getElementById('works');
 const popupOverlay = document.querySelector('#popup-overlay');
-const closeBtn = document.querySelector('.close-btn');
-const popupHeader = document.querySelector('#popup-content h2');
-const popupImg = document.querySelector('#popup-img');
-const popupDescription = document.querySelector('#popup-description');
-const liveLink = document.querySelector('.live-link');
-const sourceLink = document.querySelector('.source-link');
-const skillsList = document.querySelector('#popup-skills-buttons');
-const popupBg = document.querySelector('#popup-bg');
-const popupContent = document.querySelector('#popup-content');
-
 const projectCards = [
   {
     name: 'Tonic',
@@ -83,22 +71,24 @@ projectCards.forEach((project) => {
     .join('')}</ul> <button class='enabled-btn'>See Project</button> </div>`;
   projectsDiv.appendChild(projectCard);
 });
-
+// eslint-disable-next-line no-unused-vars
 const closeModal = () => {
-  popupOverlay.style.display = 'none'
-}
+  popupOverlay.style.display = 'none';
+};
 
 window.onclick = function modal(e) {
-  if(e.target === popupOverlay) {
-    popupOverlay.style.display = 'none'
+  if (e.target === popupOverlay) {
+    popupOverlay.style.display = 'none';
   }
-}
-function populatePopupWindow (index){
-  const myPopup = projectCards[index]
+};
+
+// Add popup modal dynamically
+function populatePopupWindow(index) {
+  const myPopup = projectCards[index];
   popupOverlay.innerHTML = ` <div id="popup-content">
   <div id="popup-title">
     <h2>${myPopup.name}</h2>
-    <div class="close-btn">&times;</div>
+    <div class="close-btn" onclick="closeModal()">&times;</div>
   </div>
   <p class="project-info">
     <span class="project-company">CANOPY</span>
@@ -110,48 +100,25 @@ function populatePopupWindow (index){
   <img id="popup-img" src="${myPopup.image}" alt="project screenshot">
   <div id="popup-details">
     <p id="popup-description">${myPopup.description}</p>
-    <div id="popup-skills-buttons">
-      <div id="popup-buttons">
-        <a class="enabled-btn live-link" target="_blank" rel="noopener" href="${myPopup['live link']}">See Live<i
-            class="fa-solid fa-arrow-up-right-from-square"></i></a>
-        <a class="enabled-btn source-link" target="_blank" rel="noopener" href="${myPopup['source link']}">See Source<i
-            class="fa-brands fa-github"></i></a>
+    
+      <div id="btn-container">
+      <button class="btn-main"> <span class="btn-popup">See Live</span>
+      <a  target="_blank" rel="noopener" href="${myPopup['live link']}">
+
+      <i class="fa-solid fa-arrow-up-right-from-square"></i></a></button>
+        <button class="btn-main"><span class="btn-popup">See Source</span>
+        <a target="_blank" rel="noopener" href="${myPopup['source link']}"><i class="fa-brands fa-github"></i></a> </button>
       </div>
-    </div>
+     
+    
   </div>
-</div>`
-popupOverlay.style.display = 'block'
+</div>`;
+  popupOverlay.style.display = 'block';
 }
-
-// const ul = document.createElement('ul');
-// ul.className = 'project-languages';
-// skillsList.prepend(ul);
-
-// function populatePopupWindow(index) {
-//   ul.innerHTML = '';
-//   popupHeader.innerText = projectCards[index].name;
-//   popupImg.src = projectCards[index].image;
-//   popupDescription.innerText = projectCards[index].description;
-//   projectCards[index].technologies.forEach((skill) => {
-//     const li = document.createElement('li');
-//     li.className = 'language';
-//     li.innerText = skill;
-//     ul.appendChild(li);
-//   });
-//   liveLink.href = projectCards[index]['live link'];
-//   sourceLink.href = projectCards[index]['source link'];
-// }
 
 const projectBtn = document.querySelectorAll('.project .enabled-btn');
 projectBtn.forEach((btn) => btn.addEventListener('click', () => {
   popupOverlay.classList.add('active');
   body.classList.add('active');
-  popupBg.classList.add('active');
   populatePopupWindow(Array.prototype.indexOf.call(projectBtn, btn));
 }));
-
-closeBtn.addEventListener('click', () => {
-  popupContent.style.display = 'none'
-  body.classList.remove('active');
-  popupBg.classList.remove('active');
-});
